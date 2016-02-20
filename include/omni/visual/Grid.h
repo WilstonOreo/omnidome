3170243f6d5ec8f71873d8bfe88d5f10e3de508a
Michael Winkelmann <m@cr8tr.org>
2016-02-20 12:58:13 +0100

    Visual Grid

---
+++ include/omni/visual/Grid.h
@@ -0,0 +1,61 @@
+/* Copyright (c) 2014-2015 "Omnidome" by cr8tr
+ * Dome Mapping Projection Software (http://omnido.me).
+ * Omnidome was created by Michael Winkelmann aka Wilston Oreo (@WilstonOreo)
+ *
+ * This file is part of Omnidome.
+ *
+ * Omnidome is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+ #ifndef OMNI_VISUAL_GRID_H_
+ #define OMNI_VISUAL_GRID_H_
+
+#include <memory>
+#include <QSize>
+#include <omni/visual/Interface.h>
+#include <omni/visual/Camera.h>
+#include <omni/visual/Plane.h>
+
+class QOpenGLShaderProgram;
+
+namespace omni {
+    namespace visual {
+        /**@brief A cartesian grid with lining adapted to distance
+         **/
+        class Grid : public visual::Interface {
+        public:
+            Grid(Camera const&);
+            ~Grid();
+
+            /// Draw grid
+            void draw() const;
+
+            /// Update grid mesh
+            void update();
+
+            /**@brief Resolution
+             **/
+            void setResolution(QSize _size);
+
+            /// Return resolution
+            QSize const& resolution() const;
+
+        private:
+            QSize resolution_;
+            Camera const& camera_;
+            Plane plane_;
+            std::unique_ptr<QOpenGLShaderProgram> shader_;
+        };
+    }
+}
+
+#endif /* OMNI_VISUAL_GRID_H_ */
---
+++ src/lib/visual/Grid.cpp
@@ -0,0 +1,71 @@
+/* Copyright (c) 2014-2015 "Omnidome" by cr8tr
+ * Dome Mapping Projection Software (http://omnido.me).
+ * Omnidome was created by Michael Winkelmann aka Wilston Oreo (@WilstonOreo)
+ *
+ * This file is part of Omnidome.
+ *
+ * Omnidome is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <omni/visual/Grid.h>
+#include <omni/util.h>
+#include <QOpenGLShaderProgram>
+
+namespace omni {
+    namespace visual {
+        Grid::Grid(Camera const& _camera) :
+            camera_(_camera) {
+            setResolution(QSize(1024,1024));
+            plane_.resize(2,2);
+        }
+
+        Grid::~Grid() {
+        }
+
+        void Grid::draw() const {
+
+            shader_->bind();
+            shader_->setUniformValue("resolution",
+                GLfloat(resolution_.width()),
+                GLfloat(resolution_.height()));
+            shader_->setUniformValue("color",1.0,1.0,1.0,1.0);
+            shader_->setUniformValue("eye_pos",camera_.eye());
+            shader_->setUniformValue("dir",camera_.direction().vec());
+
+            plane_.draw();
+            shader_->release();
+        }
+
+        void Grid::update() {
+            if (!shader_) {
+                using omni::util::fileToStr;
+                static QString _vertSrc = fileToStr(":/shaders/grid.vert");
+                static QString _fragmentSrc = fileToStr(":/shaders/grid.frag");
+                shader_.reset(new QOpenGLShaderProgram());
+                shader_->addShaderFromSourceCode(QOpenGLShader::Vertex,_vertSrc);
+                shader_->addShaderFromSourceCode(QOpenGLShader::Fragment,_fragmentSrc);
+                shader_->link();
+            }
+
+            plane_.update();
+        }
+
+        QSize const& Grid::resolution() const {
+            return resolution_;
+        }
+
+        void Grid::setResolution(QSize _resolution) {
+            resolution_ = _resolution;
+        }
+    }
+}
