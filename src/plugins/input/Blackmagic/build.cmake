08ac93169ddb640ad067755f30d1b85ce1d5db30
Michael Winkelmann <michael@winkelmann.site>
2016-12-19 22:49:50 +0100

    Integrate DISABLE_CONTEXT_SWITCH into build system

--- src/plugins/input/Blackmagic/build.cmake
+++ src/plugins/input/Blackmagic/build.cmake
@@ -5,7 +5,12 @@ omni_option(
     ${CMAKE_SOURCE_DIR}/../Blackmagic_DeckLink_SDK
 )
 
-if (${CMAKE_SYSTEM_NAME} MATCHES "Linux") 
+if (NOT EXISTS ${Blackmagic_PATH} OR NOT IS_DIRECTORY ${Blackmagic_PATH})
+  SET(plugin_input_Blackmagic_IGNORE TRUE)
+  return()
+endif()
+
+if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
   set(Blackmagic_SDK_PATH ${Blackmagic_PATH}/Linux)
 endif()
 
